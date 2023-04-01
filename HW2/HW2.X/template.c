68db65cec0e79b314b03a6fcffa72d0826a2211f
ianshi8 <ianshi@u.northwestern.edu>
2023-04-01 10:30:45 -0500

    HW2 Code

--- HW2/HW2.X/template.c
+++ HW2/HW2.X/template.c
@@ -4,14 +4,21 @@ void blink(int, int); // blink the LEDs function
 
 int main(void) {
   char message[100];
+  int num_blinks = 0;
+  int blink_time = 0;
   
   NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
   while (1) {
     NU32DIP_ReadUART1(message, 100); // wait here until get message from computer
+    sscanf(message, "%d", &num_blinks);
+    sprintf(message, "%d\r\n", num_blinks);
     NU32DIP_WriteUART1(message); // send message back
-    NU32DIP_WriteUART1("\r\n"); // carriage return and newline
+    NU32DIP_ReadUART1(message ,100);
+    sscanf(message, "%d", &blink_time);
+    sprintf(message, "%d\r\n", blink_time);
+    NU32DIP_WriteUART1(message);
 	if (NU32DIP_USER){
-		blink(5, 500); // 5 times, 500ms each time
+        blink(num_blinks, blink_time);
 	}
   }
 }
