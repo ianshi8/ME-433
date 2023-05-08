6c6201d0ed8058f1f6b3c97737184a3a86a1480d
ianshi8 <ianshi@u.northwestern.edu>
2023-05-07 21:06:08 -0500

    Fixed hw10 code and added demo

--- HW10/HW10.X/main.c
+++ HW10/HW10.X/main.c
@@ -8,44 +8,62 @@ int main(void) {
     ws2812b_setup();
     
     // setting hues
-    int i=0; //red
-    int j=20;
-    int k=40;
-    int l=60; // green
-    int numLEDs = 4;
-    wsColor c[numLEDs];
-    while(1) {
-        c[0]=HSBtoRGB(180,1,1);
-        c[1]=HSBtoRGB(180,1,1);
-        c[2]=HSBtoRGB(180,1,1);
-        c[3]=HSBtoRGB(180,1,1);
-        ws2812b_setColor(c, numLEDs);
-        /*
-        c[0]=HSBtoRGB(i,1,1);
-        c[1]=HSBtoRGB(j,1,1);
-        c[2]=HSBtoRGB(k,1,1);
-        c[3]=HSBtoRGB(l,1,1);
+    float hue1 = 0;
+    float hue2 = 72;
+    float hue3 = 144;
+    float hue4 = 216;
+    float hue5 = 288;
+    
+    float b1 = 0.1;
+    float b2 = 0.3; 
+    float b3 = 0.5; 
+    float b4 = 0.7;
+    float b5 = 0.9;
+    
+    float i1=  0.0011;
+    float i2=  0.0014;
+    float i3=  0.0013;
+    float i4=  0.0012;
+    float i5=  0.00123;
+
+    while(1){ 
         
-        i++;
-        j++;
-        k++;
-        l++;      
-        if (i-360>0){
-            i=0;
+        wsColor col1 = HSBtoRGB(hue1, 0.8, b1);
+        wsColor col2 = HSBtoRGB(hue2, 0.8, b2);
+        wsColor col3 = HSBtoRGB(hue3, 0.8, b3);
+        wsColor col4 = HSBtoRGB(hue4, 0.8, b4);
+        wsColor col5 = HSBtoRGB(hue5, 0.8, b5);
+        
+        wsColor cols[5] = {col1, col2, col3, col4, col5};
+        
+        b1=b1+i1;
+        b2=b2+i2;
+        b3=b3+i3;
+        b4=b4+i4;
+        b5=b5+i5;
+        
+        if(b1>0.999 || b1<0){
+            i1 = -i1;
         }
-        if(j-360>0){
-            j=0;
+        if(b2>0.999 || b2<0){
+            i2 = -i2;
         }
-        if(k-360>0){
-            k=0;
+           if(b3>0.999 || b3<0){
+            i3 = -i3;
         }
-        if(l-360>0){
-            l=0;
+         if(b4>0.999 || b4<0){
+            i4 = -i4;
         }
-        ws2812b_setColor(c, numLEDs);
-         */
-    } 
+        if(b5>0.999 || b5<0){
+            i5 = -i5;
+        }
+        
+        ws2812b_setColor(cols, 5); 
+        
+        
+    }    
 }
+
 
 void delay(void){
     float time = 0.05;
--- HW10/HW10.X/ws2812b.c
+++ HW10/HW10.X/ws2812b.c
@@ -35,7 +35,7 @@ void ws2812b_setColor(wsColor * c, int numLEDs) {
         // loop through each color bit, MSB first
         for (j = 7; j >= 0; j--) {
             // if the bit is a 1
-            if ((c[i].r << j & 0b1) == 0b1) { // identify the bit in c[].r, is it 1
+            if ((c[i].r >> j & 0b01) == 1) { // identify the bit in c[].r, is it 1
                 // the high is longer
                 delay_times[nB] = delay_times[nB - 1] + HIGHTIME;
                 nB++;
@@ -54,7 +54,7 @@ void ws2812b_setColor(wsColor * c, int numLEDs) {
         // do it again for green
         for (j = 7; j >= 0; j--) {
             // if the bit is a 1
-            if ((c[i].g << j & 0b1) == 0b1) { // identify the bit in c[].r, is it 1
+            if ((c[i].g >> j & 0b01) == 1) { // identify the bit in c[].r, is it 1
                 // the high is longer
                 delay_times[nB] = delay_times[nB - 1] + HIGHTIME;
                 nB++;
@@ -73,7 +73,7 @@ void ws2812b_setColor(wsColor * c, int numLEDs) {
 		// do it again for blue
         for (j = 7; j >= 0; j--) {
             // if the bit is a 1
-            if ((c[i].b << j & 0b1) == 0b1) { // identify the bit in c[].r, is it 1
+            if ((c[i].b >> j & 0b01) == 1) { // identify the bit in c[].r, is it 1
                 // the high is longer
                 delay_times[nB] = delay_times[nB - 1] + HIGHTIME;
                 nB++;
